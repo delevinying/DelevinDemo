48e297ba21a2f7707383d453fabc70c4ce5084dd
yyc <delevinyyc@gmail.com>
2018-10-31 16:06:30 +0800

    star

---
+++ Assets/Demo3.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Rendering;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+public class Demo3 : MonoBehaviour {
+	public static EntityArchetype entityArchetype;
+	public EntityManager entityManager;
+	public Mesh mesh;
+	public Material material;
+	public Entity entities;
+
+	public int starNum;
+	public int plane;
+	
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	public static void initialization(){
+		EntityManager _entityManager = World.Active.GetOrCreateManager<EntityManager>();
+		entityArchetype = _entityManager.CreateArchetype(
+			typeof(Position)
+		);
+	}
+	// Use this for initialization
+	// private Entity entities;
+	void Start () {
+		entityManager = World.Active.GetOrCreateManager<EntityManager>();
+		for(int i=0;i<starNum;i++){
+			entities = entityManager.CreateEntity(entityArchetype);
+			entityManager.SetComponentData(entities,new Position{Value = new int3(i*3,0,0)});
+			// entityManager.AddComponent(entities, r);
+			entityManager.AddSharedComponentData(entities,new MeshInstanceRenderer{
+				mesh = mesh,
+				material = material
+			});
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ECS/scenes/p3/RotatorSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+public class RotatorSystem : ComponentSystem {
+	struct Group{
+		public Transform transform;
+		public Rotator rotator;
+	}
+
+	protected override void OnUpdate(){
+		float del = Time.deltaTime;
+		foreach(var entity in GetEntities<Group>()){
+			entity.transform.rotation *= Quaternion.AngleAxis(entity.rotator.speed*del,Vector3.up);
+		}
+	}
+}
